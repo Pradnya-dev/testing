52f0556a32f5185f2318cd470520d6516dad1a00
Pradnya <pradnya@dhitechnologies.org>
2021-07-14 15:13:23 +0530

    handled key in promises

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
+// function indhiThirdParty(clientId, redirectURI, key){
+//     window.open(redirectURI+clientId,"myWindow","width=500, height=500");
+
+//     var promise = new Promise(function(resolve, reject){
+//         if (key){
+//             resolve('success');
+//         }
+//         else{
+//             reject('failure');
+//         }
+//     })
+
+//     promise.then((message)=>{
+//         //sending auth code 
+//         key = this.key
+//         console.log(message);
+//     }).
+//     catch((message)=>{
+       
+//         //send failure msg
+//         console.log("failure")
+//     })
+// }
+
+
 function indhiThirdParty(clientId, redirectURI, key){
     window.open("http://localhost:4203/signin/:"+clientId,"myWindow","width=800, height=800");
 
     var promise = new Promise(function(resolve, reject){
-        if (key === 'some input'){
-            resolve('success');
+        if (key.length === 40){
+                    console.log(key.length)
+                    const alphanumeric = /^[a-z0-9]+$/i;
+                    var valid = (alphanumeric.test(key));
+                    console.log(valid);
+                    resolve(key);
         }
         else{
-            reject('failure');
+            reject('failure - inavalid key');
         }
     })
 
-    promise.then((message)=>{
+    promise.then((key)=>{
         //sending auth code 
-        console.log(message);
+        console.log(key);
+        return key;
     }).
     catch((message)=>{
        
         //send failure msg
+        return message;
         console.log("failure")
     })
 }
+
 
 module.exports.indhiThirdParty = indhiThirdParty;
